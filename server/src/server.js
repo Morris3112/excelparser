1249479e046e0e2e073f63d03998115f258e8551
morris3112 <morris.beek@widgetbrain.com>
2017-09-14 10:05:29 +0200

    deleted ESlint, install bodyparser morgan and cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+//app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+  res.send('Hello');
+});
+
+app.listen(process.env.PORT || 3000);
